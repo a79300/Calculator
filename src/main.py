29fb67bd1d24c35a472fe6bf0d6d3959c3797b1b
Joao Sousa <a79300@ualg.pt>
2025-02-20 13:50:53 +0000

    Calculator First Design

--- src/main.py
+++ src/main.py
@@ -1,26 +1,117 @@
 import flet as ft
+import math
+import re
+
+
+class CalculatorApp:
+    def __init__(self, page: ft.Page):
+        self.buttons = [
+            ["COS", "SEN", "TAN", "DEL"],
+            ["√", "(", ")", "AC"],
+            ["*", "/", "^", "+/-"],
+            ["7", "8", "9", "%"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "+"],
+            ["0", ".", "ANS", "="],
+        ]
+        self.page = page
+        self.columns = []
+
+        self.expression = ft.TextField(
+            hint_text="...",
+            color="black",
+            text_align=ft.TextAlign.RIGHT,
+            read_only=True,
+            expand=True,
+            border_width=0
+        )
+
+        self.result = ft.TextField(
+            hint_text="0",
+            color="black",
+            text_align=ft.TextAlign.RIGHT,
+            read_only=True,
+            expand=True,
+            border_width=0
+        )
+        self.result_size = None
+        self.expression_size = None
+        self.current_expression = ""
+        self.display_expression = ""
+        self.last_result = ""
+
+        for row_idx, row in enumerate(self.buttons):
+            for col_idx, button_text in enumerate(row):
+                self.columns.append(
+                    ft.Container(
+                        ft.Button(
+                            button_text,
+                            bgcolor=ft.Colors.BLACK,
+                            color=ft.Colors.WHITE,
+                        ),
+                        col={
+                            "xs": 3,
+                            "sm": 3,
+                            "md": 3,
+                            "xl": 3,
+                        },
+                    )
+                )
+
+        self.main_container = ft.Column(
+            [
+                ft.ResponsiveRow(
+                    [
+                        ft.Column(
+                            [
+                                ft.ResponsiveRow(
+                                    [
+                                        ft.Column(
+                                            [
+                                                ft.IconButton(
+                                                    icon=ft.Icons.MENU,
+                                                    icon_color="black"
+                                                )
+                                            ],
+                                            col={
+                                                "xs": 3,
+                                                "sm": 3,
+                                                "md": 3,
+                                                "xl": 3,
+                                            },
+                                        ),
+                                        ft.Column(
+                                            [self.expression],
+                                            col={
+                                                "xs": 9,
+                                                "sm": 9,
+                                                "md": 9,
+                                                "xl": 9,
+                                            },
+                                        ),
+                                    ]
+                                ),
+                                ft.ResponsiveRow([self.result]),
+                            ]
+                        )
+                    ]
+                ),
+                ft.ResponsiveRow(self.columns),
+            ]
+        )
+
+        self.stack = ft.Stack(
+            controls=[
+                self.main_container,
+            ],
+            expand=True,
+        )
+
+        self.page.add(self.stack)
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
-    )
+    CalculatorApp(page)
 
 
 ft.app(target=main, assets_dir="assets")
